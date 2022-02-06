b531fbc5e170fcdf6ad7cb35be5135bf02dba970
高冠祺 <307590625@qq.com>
2022-02-06 22:21:39 +0800

    积分列表

--- src/api/core/integral-grade.js
+++ src/api/core/integral-grade.js
@@ -1,8 +1,17 @@
 import request from '@/utils/request'
 
+// 接口测试
 export function hello() {
   return request({
     url: '/admin/core/integralGrade/hello',
+    method: 'get',
+  })
+}
+
+// 积分列表
+export function getList() {
+  return request({
+    url: '/admin/core/integralGrade/list',
     method: 'get',
   })
 }
--- src/views/core/integral-grade/list.vue
+++ src/views/core/integral-grade/list.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    积分列表
+    <el-table :data="list" border stripe style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+
+      <el-table-column
+        prop="borrowAmount"
+        label="借款额度">
+      </el-table-column>
+
+      <el-table-column
+        prop="integralStart"
+        label="积分区间开始">
+      </el-table-column>
+
+      <el-table-column
+        prop="integralEnd"
+        label="积分区间结束">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import { getList } from '@/api/core/integral-grade'
+
 export default {
-  name: 'list'
+  name: 'list',
+  data() {
+    return {
+      loading: false,
+      list: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      getList().then(response => {
+        this.list = response.data.list
+      }).finally(() =>{
+        this.loading = false
+      })
+    }
+  }
 }
 </script>
 
